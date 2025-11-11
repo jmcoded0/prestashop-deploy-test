7e52bf8a4d2d8489396b7852f49641942b25037c
Johnson Matthew <88945266+jmcoded0@users.noreply.github.com>
2025-11-11 21:42:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-echo "PrestaShop Deployment Placeholder - Test Submission";
+echo "<h1 style='color:green;text-align:center;margin-top:50px;'>🚀 PHP Deployment Successful on Render!</h1>";
+echo "<p style='text-align:center;'>Your PHP server is running perfectly.</p>";
 ?>
